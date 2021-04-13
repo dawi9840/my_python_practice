9255c939bdf92d1b50b1bd51c5ec32ad6a633bc9
Dawi Lu <dawi9840@gmail.com>
2021-04-13 10:48:53 +0800

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import geometry.point
-result = geometry.point.distance(3, 4) 
-print("distance = ", result)
-
-import geometry.line as line
-result2 = line.slope(1, 1, 3, 3)
-print("slope = ", result2)
