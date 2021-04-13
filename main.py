76198741238aed606284b96cc159d596265b8b44
Dawi Lu <dawi9840@gmail.com>
2021-04-13 10:47:54 +0800

    Add files via upload
    Use main.py to call geometry folder file.

---
+++ main.py
@@ -0,0 +1,7 @@
+import geometry.point
+result = geometry.point.distance(3, 4) 
+print("distance = ", result)
+
+import geometry.line as line
+result2 = line.slope(1, 1, 3, 3)
+print("slope = ", result2)
